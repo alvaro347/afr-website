f089a4f44172eae37374d78f8c51fcb3d956a5c6
Alvaro <alvaro347@gmail.com>
2018-04-22 13:22:44 +0800

    Added Gulp configuration, jasmine, browser sync and Sass

---
+++ server/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from flask import redirect, jsonify, url_for, flash
+from flask import session as login_session
+from flask import make_response
+
+# from sqlalchemy import create_engine, asc
+# from sqlalchemy.orm import sessionmaker
+# from database_setup import Base, Gallery, Pictures, User
+# from oauth2client.client import flow_from_clientsecrets
+# from oauth2client.client import FlowExchangeError
+# from werkzeug.utils import secure_filename
+
+# import httplib2
+# import requests
+# import random
+# import string
+# import json
+# import os
+
+app = Flask(
+    __name__, static_folder='../static', template_folder='../static'
+    )
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
